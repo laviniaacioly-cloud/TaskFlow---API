c8f89ca234659eb26f9b861959673af8c25467f3
laviniaacioly-cloud <lavinia.acioly@aluno.senai.br>
2026-09-02 11:28:47 -0300

    inicio do dia 4

---
+++ taskflow-api/src/controllers/projetos.controller.js
@@ -0,0 +1,25 @@
+let projetos = [
+    {id: 1, nome: "Projeto Y", decricao: "descrição do projeto Y"},
+    {id: 2, nome: "Projeto X", decricao: "descrição do projeto X"},
+    {id: 3, nome: "Projeto Z", decricao: "descrição do projeto Z"},
+];
+
+let proximoProjeto = 4
+
+const projetosCoontroller = {
+    listarProjetos(req, res) {
+        
+    },
+    buscarProjetosId(req, res) {
+
+    },
+    criarProjetos(req, res) {
+
+    },
+    atualizarProjetos(req, res) {
+
+    },
+    removerProjetos(req, res) {
+
+    },
+};
--- taskflow-api/src/routes/tarefas.routes.js
+++ taskflow-api/src/routes/tarefas.routes.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const tarefasController = require("../controllers/tarefas.controller");
+const tarefasController = require('../controllers/tarefas.controller');
 
 // app.use(express.json());
 
 // GETs - LISTAR TAREFAS
-router.get("/", tarefasController.listar);
+router.get('/', tarefasController.listar);
 
 // CRIAR TAREFA - POST
-router.post("/", tarefasController.criar);
+router.post('/', tarefasController.criar);
 
 //  ROTAS DE ESATATISTICAS
 router.get("/estatisticas", tarefasController.estatisticas);
 router.get("/estatisticas/resumo", tarefasController.estatisticasResumo);
 
 // EDITAR TAREFAS - PUT
-router.put("/:id", tarefasController.atualizar);
+router.put('/:id', tarefasController.atualizar);
 
 // DELETAR TAREFAS - DELETE
-router.delete("/:id", tarefasController.remover);
+router.delete('/:id', tarefasController.remover);
 
 // BUSCAR POR ID
-router.get("/:id", tarefasController.buscarPorId);
+router.get('/:id', tarefasController.buscarPorId);
 
 //EXPORTR ROTAS E TAREFAS
 module.exports = router;
