473168602d6dc28dba489e7fbc7ca90fc164d1d3
laviniaacioly-cloud <lavinia.acioly@aluno.senai.br>
2026-09-03 08:09:43 -0300

    projetos controller

--- taskflow-api/server.js
+++ taskflow-api/server.js
@@ -3,11 +3,14 @@
 const express = require("express");
 const tarefasRoutes = require("./src/routes/tarefas.routes");
 const usuariosRoutes = require("./src/routes/usuarios.routes");
+const projetosRoutes = require("./src/routes/projetos.routes");
 const app = express();
 const PORTA = 3000;
 
 // Permite que o Express receba dados em JSON
 app.use(express.json());
+
+
 
 //  ROTA INICIAL DA API
 app.get("/", (req, res) => {
@@ -19,6 +22,8 @@ app.use("/usuarios", usuariosRoutes);
 
 // ROUTER.GET VIRA GET /TAREFAS - TAREFAS ROUTES
 app.use("/tarefas", tarefasRoutes);
+
+app.use("/projetos", projetosRoutes);
 
 //  Rota 404 — DEVE SER A ÚLTIMA SEMPRE
 app.use((req, res) => {
--- taskflow-api/src/controllers/projetos.controller.js
+++ taskflow-api/src/controllers/projetos.controller.js
@@ -1,25 +1,70 @@
 let projetos = [
-    {id: 1, nome: "Projeto Y", decricao: "descrição do projeto Y"},
-    {id: 2, nome: "Projeto X", decricao: "descrição do projeto X"},
-    {id: 3, nome: "Projeto Z", decricao: "descrição do projeto Z"},
+  { id: 1, nome: "Projeto Y", descricao: "descrição do projeto Y" },
+  { id: 2, nome: "Projeto X", descricao: "descrição do projeto X" },
+  { id: 3, nome: "Projeto Z", descricao: "descrição do projeto Z" },
 ];
 
-let proximoProjeto = 4
+let proximoProjeto = 4;
 
-const projetosCoontroller = {
-    listarProjetos(req, res) {
-        
-    },
-    buscarProjetosId(req, res) {
+const projetosController = {
+  // GET - MOSTRAR PROJETOS
+  listarProjetos(req, res) {
+    res.json(projetos);
+  },
+  // GET - BUSCAR POR ID
+  buscarProjetosId(req, res) {
+    const id = Number(req.params.id);
+    const projeto = projetos.find((p) => p.id === id);
+    if (!projeto) {
+      return res.status(404).json({ erro: "Projeto não encontrada" });
+    }
+    // Se encontrou — retornar a tarefa
+    res.json(projeto);
+  },
+  // POST - CRIAR NOVOS PROJETOS
+  criarProjetos(req, res) {
+    const { nome, descricao } = req.body;
+    const novoProjeto = {
+      id: proximoProjeto,
+      nome,
+      descricao,
+    };
+    projetos.push(novoProjeto);
+    proximoProjeto++;
 
-    },
-    criarProjetos(req, res) {
+    res.status(201).json(novoProjeto);
+  },
+  // PUT - EDITAR PROJETOS
+  atualizarProjetos(req, res) {
+    const id = Number(req.params.id);
+    const projeto = projetos.find((p) => p.id === id);
 
-    },
-    atualizarProjetos(req, res) {
+    if (!projeto) {
+      return res.status(404).json({
+        mensagem: "Projeto não encontrado",
+      });
+    }
+    const { nome, descricao } = req.body;
+    projeto.nome = nome ?? projeto.nome;
+    projeto.descricao = descricao ?? projeto.descricao;
 
-    },
-    removerProjetos(req, res) {
+    res.json(projeto);
+  },
+  // DELETE - APAGAR PROJETOS
+  removerProjetos(req, res) {
+    const id = Number(req.params.id);
+    const indice = projetos.findIndex((p) => p.id === id);
 
-    },
+    if (indice === -1) {
+      return res.status(404).json({
+        mensagem: "Projeto não encontrado",
+      });
+    }
+    const projetoRemovido = projetos.splice(indice, 1);
+    res.json({
+      mensagem: "Projeto removido com sucesso",
+      projeto: projetoRemovido[0],
+    });
+  },
 };
+module.exports = projetosController;
